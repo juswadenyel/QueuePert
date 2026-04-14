8135f0e722d6abf950ade3fb566b66f2e15b336a
rathulf <ralphsampan@gmail.com>
2026-04-14 14:58:01 +0800

    wla ra ni

--- app-frontend/src/index.js
+++ app-frontend/src/index.js
@@ -8,7 +8,7 @@ import Dashboard from './modules/admin/Dashboard';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Dashboard />
+    <App />
   </React.StrictMode>
 );
 
