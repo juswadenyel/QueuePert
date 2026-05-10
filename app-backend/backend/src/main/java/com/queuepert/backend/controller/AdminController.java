c32ee915bf6ae2c9c86883392847f7d568438d16
unknown <hammermikuzen@gmail.com>
2026-05-10 20:10:39 +0800

    admin login backend complete

--- app-backend/backend/src/main/java/com/queuepert/backend/controller/AdminController.java
+++ app-backend/backend/src/main/java/com/queuepert/backend/controller/AdminController.java
@@ -1,5 +1,34 @@
 package com.queuepert.backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.queuepert.backend.entity.AdminEntity;
+import com.queuepert.backend.entity.StudentEntity;
+import com.queuepert.backend.service.AdminService;
+import com.queuepert.backend.service.StudentService;
+
+@RestController
+@RequestMapping("/admin")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AdminController {
+
+    AdminService adminService;
+
+     public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/login")
+    public String loginAdmin(@RequestBody AdminEntity admin) {
+
+        return adminService.loginAdmin(
+            admin.getUniversityEmail(),
+            admin.getPassword()
+        );
+    }
 
 }
